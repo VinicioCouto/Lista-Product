0f3f63faada6004a00dbc4cb6c0d74fd88a6baf7
Vinicio Couto <viniciogbi@gmail.com>
2025-01-27 16:44:18 -0300

    mudança de button concluida

---
+++ javascripit/index.js
@@ -0,0 +1,8 @@
+function MudarQuantidade() {
+    const AddCarinho = document.getElementById('add-carrinho')
+    const Quantidade = document.getElementById('quantitade')
+
+    AddCarinho.style.opacity = '0.0'
+    Quantidade.style.opacity = '1.0'
+    Quantidade.style.zIndex = '99';
+}
