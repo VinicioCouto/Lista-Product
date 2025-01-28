53e34dd63858b194465445b2748258a14867d9d2
Vinicio Couto <viniciogbi@gmail.com>
2025-01-28 10:36:09 -0300

    adicionar produtos

--- javascripit/index.js
+++ javascripit/index.js
@@ -1,3 +1,47 @@
-const addCarinhos = document.querySelectorAll('.add-carrinho')
-const quantidades = document.querySelectorAll('.quantidade')
+const addCarrinhos = document.querySelectorAll('.add-carrinho');
+const quantidades = document.querySelectorAll('.quantitade'); 
 
+addCarrinhos.forEach((addCarinho, index) => {
+    const quantidade = quantidades[index];
+    const numeroQuantidade = quantidade.querySelector('.numero-quantidade');
+    const buttonMais = quantidade.querySelector('.mais-button');
+    const buttonMenos = quantidade.querySelector('.menos-button');
+
+    
+    buttonMais.addEventListener('click', () => {
+        let quantidadeAtual = parseInt(numeroQuantidade.textContent);
+        numeroQuantidade.textContent = quantidadeAtual + 1;
+    });
+
+
+    buttonMenos.addEventListener('click', () => {
+        let quantidadeAtual = parseInt(numeroQuantidade.textContent);
+
+        if (quantidadeAtual > 1) {
+            numeroQuantidade.textContent = quantidadeAtual - 1;
+        } else {
+            numeroQuantidade.textContent = 0;
+            mudarQuantidade(addCarinho, quantidade, numeroQuantidade);
+        }
+    });
+
+    
+    addCarinho.addEventListener('click', () => {
+        mudarQuantidade(addCarinho, quantidade, numeroQuantidade);
+    });
+});
+
+function mudarQuantidade(addCarinho, quantidade, numeroQuantidade) {
+    if (parseInt(numeroQuantidade.textContent) === 0) {
+        addCarinho.style.opacity = '1.0';
+        quantidade.style.opacity = '0.0';
+        addCarinho.style.zIndex = '99';
+        quantidade.style.zIndex = '0';
+        numeroQuantidade.textContent = 1;
+    } else {
+        addCarinho.style.opacity = '0.0';
+        quantidade.style.opacity = '1.0';
+        quantidade.style.zIndex = '99';
+        addCarinho.style.zIndex = '0';
+    }
+}
